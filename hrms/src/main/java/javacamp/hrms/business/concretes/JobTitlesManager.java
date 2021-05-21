3a10225eb1f5c22de450aea17a65290c68198b06
KARACA <KARACA@DESKTOP-F7PQMCT>
2021-05-21 17:57:47 +0300

    javacamp

---
+++ hrms/src/main/java/javacamp/hrms/business/concretes/JobTitlesManager.java
@@ -0,0 +1,36 @@
+package javacamp.hrms.business.concretes;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javacamp.hrms.business.abstracts.JobTitlesService;
+import javacamp.hrms.dataAccess.abstracts.JobTitlesDao;
+import javacamp.hrms.entities.concretes.JobTitles;
+
+@Service
+public class JobTitlesManager implements JobTitlesService{
+
+	private JobTitlesDao jobTitlesDao;
+	
+	@Autowired
+	
+	public JobTitlesManager(JobTitlesDao jobTitlesDao) {
+		super();
+		this.jobTitlesDao = jobTitlesDao;
+	}
+
+	@Override
+	public List<JobTitles> getAll() {
+		return jobTitlesDao.findAll();
+	}
+		
+	}
+	
+		
+		
+	
+
+
---
+++ hrms/src/main/java/javacamp/hrms/dataAccess/abstracts/JobTitlesDao.java
@@ -0,0 +1,13 @@
+package javacamp.hrms.dataAccess.abstracts;
+
+
+	import org.springframework.data.jpa.repository.JpaRepository;
+	import javacamp.hrms.entities.concretes.JobTitles;
+
+	public interface JobTitlesDao extends JpaRepository <JobTitles, Integer> {
+		
+	}
+	
+
+
+
